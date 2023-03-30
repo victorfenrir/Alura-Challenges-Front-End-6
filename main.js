de1cd4ee3167dcc15d8ef9b78e2a1d7d0d8fa728
Victor Barra <victorbarrapereira@gmail.com>
2023-03-30 18:54:59 -0300

    progresso - dia 11 ~ Correções no style.css

--- main.js
+++ main.js
@@ -124,12 +124,35 @@ camposDeEntrada.forEach(function(campo) {
   });
 });
 
-// adicione um evento de clique ao botão
-botaoAvancar.addEventListener('click', function(event) {
-  // previna o envio do formulário
-  event.preventDefault();
+//Validar data de nascimento
 
-  // retorne false para impedir o envio do formulário
+function validadata(){
+  var data = document.getElementById("nascimento").value; // pega o valor do input
+  data = data.replace(/\//g, "-"); // substitui eventuais barras (ex. IE) "/" por hífen "-"
+  var data_array = data.split("-"); // quebra a data em array
+  
+  // para o IE onde será inserido no formato dd/MM/yyyy
+  if(data_array[0].length != 4){
+     data = data_array[2]+"-"+data_array[1]+"-"+data_array[0]; // remonto a data no formato yyyy/MM/dd
+  }
+  
+  // comparo as datas e calculo a idade
+  var hoje = new Date();
+  var nasc  = new Date(data);
+  var idade = hoje.getFullYear() - nasc.getFullYear();
+  var m = hoje.getMonth() - nasc.getMonth();
+  if (m < 0 || (m === 0 && hoje.getDate() < nasc.getDate())) idade--;
+  
+  if(idade < 12){
+     document.getElementById("nasc-error").style.display = "block";
+     return false;
+  }
+
+  if(idade >= 13){
+    return true;
+ }
+  
+  // se for maior que 60 não vai acontecer nada!
   return false;
-});
+}
 
